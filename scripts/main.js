88dda35807008ee9d005d840feb6208fd13bf93e
GenericSomeone <80782836+GenericSomeone@users.noreply.github.com>
2021-06-12 17:19:34 -0700

    added dart vader

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
-//why bother
+//I have no idea why this is better than putting a line of require()s but I'll do it anyway
 let scripts = [
   "nukeunits",
   "amogus",
   "snas",
+  "dartvader",
 ];
 
 scripts.forEach(e => {
